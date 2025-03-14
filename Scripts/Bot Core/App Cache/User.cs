4dd2e139bfaa9d73efdac402ee3a6980ecf0601f
Stone Hobbs <stoneh826@gmail.com>
2025-03-14 17:21:43 -0500

    Refactor user

--- Scripts/Bot Core/App Cache/User.cs
+++ Scripts/Bot Core/App Cache/User.cs
@@ -9,14 +9,38 @@
 
         public static async Task<User?> CreateBot() {
             Logger.Info("Creating bot user.");
+
             var clientWrapper = await AppCache.ChatterClientWrapper.Get();
-            return clientWrapper is null ? null : await Create(clientWrapper);
+            if (clientWrapper is null) {
+                Logger.Warning("Could not create bot user because chatter client wrapper get attempt failed.");
+                return null;
+            }
+
+            var createdUser = await Create(clientWrapper);
+            if (createdUser is null) {
+                Logger.Warning("Could not create bot user because create attempt failed.");
+                return null;
+            }
+
+            return createdUser;
         }
 
         public static async Task<User?> CreateBroadcaster() {
             Logger.Info("Creating broadcaster user.");
+
             var clientWrapper = await AppCache.CollectorClientWrapper.Get();
-            return clientWrapper is null ? null : await Create(clientWrapper);
+            if (clientWrapper is null) {
+                Logger.Warning("Could not create broadcaster user because collector client wrapper get attempt failed.");
+                return null;
+            }
+
+            var createdUser = await Create(clientWrapper);
+            if (createdUser is null) {
+                Logger.Warning("Could not create broadcaster user because create attempt failed.");
+                return null;
+            }
+
+            return createdUser;
         }
 
         private User(UserData data) {
@@ -25,20 +49,21 @@
         }
 
         private static async Task<User?> Create(HttpClientWrapper clientWrapper) {
-            Logger.Info("Creating user.");
             var client = await clientWrapper.GetClient();
             if (client is null) {
+                Logger.Warning("Could not create user because client wrapper get client attempt failed.");
                 return null;
             }
 
             var potentialUsersData = await Util.GetMessageAs<UsersData>(TwitchAPI.GetUsers(client));
             if (potentialUsersData is null) {
+                Logger.Warning("Could not create user because Twitch get users attempt failed.");
                 return null;
             }
 
             var usersData = (UsersData)potentialUsersData;
             if (usersData.Data.Length == 0) {
-                Logger.Warning($"Cannot create user because usersData.Data.Length is 0.");
+                Logger.Warning("Could not create user because Twitch get users attempt failed.");
                 return null;
             }
 
