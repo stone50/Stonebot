5fdc33eb4e48856ef76af2fc3068f15ec372ac81
Stone Hobbs <stoneh826@gmail.com>
2025-04-07 15:57:08 -0500

    Refactor message message

--- Scripts/Message/Message.cs
+++ Scripts/Message/Message.cs
@@ -4,7 +4,7 @@
     using System.Text.RegularExpressions;
     using System.Threading.Tasks;
 
-    internal class Message(string keyword, Regex regex, Func<ChannelChatMessageEvent, PermissionLevel, Match, Task> useAction) {
+    internal class Message(string keyword, Regex regex, Func<ChannelChatMessageEvent, Match, Task> useAction) {
         public event EventHandler<PermissionLevel> PermissionLevelChanged = delegate { };
         public event EventHandler<int> UseDelayChanged = delegate { };
         public event EventHandler<bool> IsEnabledChanged = delegate { };
@@ -15,12 +15,13 @@
         public PermissionLevel PermissionLevel { get => permissionLevel; set => SetPermissionLevel(value); }
         public int UseDelay { get => useDelay; set => SetUseDelay(value); }
         public DateTime LastUsed { get; private set; }
-        public Func<ChannelChatMessageEvent, PermissionLevel, Match, Task> UseAction = useAction;
+        public Func<ChannelChatMessageEvent, Match, Task> UseAction = useAction;
 
         public bool IsReadyToUse => DateTime.Now > LastUsed.AddMilliseconds(UseDelay);
 
-        public async Task<bool> Use(ChannelChatMessageEvent messageEvent) {
+        public async Task<bool?> Use(ChannelChatMessageEvent messageEvent) {
             Logger.Info($"Using message {Keyword}.");
+
             if (!IsEnabled) {
                 return false;
             }
@@ -35,29 +36,37 @@
             }
 
             var userPermissionLevel = await Permission.GetHighest(messageEvent.ChatterUserId);
-            if (userPermissionLevel is null || userPermissionLevel < PermissionLevel) {
+            if (userPermissionLevel is null) {
+                Logger.Warning($"Could not use message {Keyword} because permission get highest attempt failed. Message event chatter user id: {messageEvent.ChatterUserId}.");
+                return null;
+            }
+
+            if (userPermissionLevel < PermissionLevel) {
                 return false;
             }
 
             LastUsed = DateTime.Now;
-            await UseAction(messageEvent, (PermissionLevel)userPermissionLevel, match);
+            await UseAction(messageEvent, match);
             return true;
         }
 
         public void SetIsEnabled(bool isEnabled) {
-            Logger.Info($"Setting is enabled for message {Keyword}.");
+            Logger.Info($"Setting is enabled for message {Keyword}. Is enabled: {isEnabled}.");
+
             this.isEnabled = isEnabled;
             Util.InvokeDeferred(IsEnabledChanged, IsEnabled);
         }
 
         public void SetPermissionLevel(PermissionLevel permissionLevel) {
-            Logger.Info($"Setting permission level for message {Keyword}.");
+            Logger.Info($"Setting permission level for message {Keyword}. Permission level: {permissionLevel}.");
+
             this.permissionLevel = permissionLevel;
             Util.InvokeDeferred(PermissionLevelChanged, PermissionLevel);
         }
 
         public void SetUseDelay(int useDelay) {
-            Logger.Info($"Setting use delay for message {Keyword}.");
+            Logger.Info($"Setting use delay for message {Keyword}. Use delay: {useDelay}.");
+
             this.useDelay = useDelay;
             Util.InvokeDeferred(UseDelayChanged, UseDelay);
         }
