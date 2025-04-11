99d4103dba915f40f08a76dfc0235fde7113b454
Stone Hobbs <stoneh826@gmail.com>
2025-04-10 20:46:24 -0500

    Rework logs in bot core app cache user

--- Scripts/Bot Core/App Cache/User.cs
+++ Scripts/Bot Core/App Cache/User.cs
@@ -1,5 +1,6 @@
 ﻿namespace Stonebot.Scripts.Bot_Core.App_Cache {
     using Models;
+    using System.Text.Json;
     using System.Threading.Tasks;
     using Twitch;
 
@@ -8,17 +9,18 @@
         public string UserName { get; private set; }
 
         public static async Task<User?> CreateBot() {
-            Logger.Info("Creating bot user.");
+            var logPrefix = $"{nameof(User)} | {nameof(CreateBot)}";
+            Logger.Info(logPrefix);
 
             var clientWrapper = await AppCache.ChatterClientWrapper.Get();
             if (clientWrapper is null) {
-                Logger.Warning("Could not create bot user because chatter client wrapper get attempt failed.");
+                Logger.Warning($"{logPrefix} | {nameof(AppCache.ChatterClientWrapper.Get)} result is null.");
                 return null;
             }
 
             var createdUser = await Create(clientWrapper);
             if (createdUser is null) {
-                Logger.Warning("Could not create bot user because create attempt failed.");
+                Logger.Warning($"{logPrefix} | {nameof(Create)} result is null.\n{nameof(clientWrapper)}: {JsonSerializer.Serialize(clientWrapper)}");
                 return null;
             }
 
@@ -26,17 +28,18 @@
         }
 
         public static async Task<User?> CreateBroadcaster() {
-            Logger.Info("Creating broadcaster user.");
+            var logPrefix = $"{nameof(User)} | {nameof(CreateBroadcaster)}";
+            Logger.Info(logPrefix);
 
             var clientWrapper = await AppCache.CollectorClientWrapper.Get();
             if (clientWrapper is null) {
-                Logger.Warning("Could not create broadcaster user because collector client wrapper get attempt failed.");
+                Logger.Warning($"{logPrefix} | {nameof(AppCache.CollectorClientWrapper.Get)} result is null.");
                 return null;
             }
 
             var createdUser = await Create(clientWrapper);
             if (createdUser is null) {
-                Logger.Warning("Could not create broadcaster user because create attempt failed.");
+                Logger.Warning($"{logPrefix} | {nameof(Create)} result is null.\n{nameof(clientWrapper)}: {JsonSerializer.Serialize(clientWrapper)}");
                 return null;
             }
 
@@ -49,21 +52,24 @@
         }
 
         private static async Task<User?> Create(HttpClientWrapper clientWrapper) {
+            var logPrefix = $"{nameof(User)} | {nameof(Create)}";
+            Logger.Info($"{logPrefix}\n{nameof(clientWrapper)}: {JsonSerializer.Serialize(clientWrapper)}");
+
             var client = await clientWrapper.GetClient();
             if (client is null) {
-                Logger.Warning("Could not create user because client wrapper get client attempt failed.");
+                Logger.Warning($"{logPrefix} | {nameof(clientWrapper.GetClient)} result is null.");
                 return null;
             }
 
             var potentialUsersData = await Util.GetMessageAs<UsersData>(TwitchAPI.GetUsers(client));
             if (potentialUsersData is null) {
-                Logger.Warning("Could not create user because Twitch get users attempt failed.");
+                Logger.Warning($"{logPrefix} | {nameof(TwitchAPI.GetUsers)} was unsuccessful.\n{nameof(client)}: {JsonSerializer.Serialize(client)}");
                 return null;
             }
 
             var usersData = (UsersData)potentialUsersData;
             if (usersData.Data.Length == 0) {
-                Logger.Warning("Could not create user because Twitch get users attempt failed.");
+                Logger.Warning($"{logPrefix} | {nameof(TwitchAPI.GetUsers)} was unsuccessful.\n{nameof(usersData)}: {JsonSerializer.Serialize(usersData)}");
                 return null;
             }
 
