f22e536210d17eada45d3d7e1ef0e97cf2d9b17c
Stone Hobbs <stoneh826@gmail.com>
2025-04-14 00:10:51 -0500

    Rework logs in command command

--- Scripts/Command/Command.cs
+++ Scripts/Command/Command.cs
@@ -1,22 +1,31 @@
 ﻿namespace Stonebot.Scripts.Command {
     using Bot_Core.Models.EventSub;
     using System;
+    using System.Text.Json;
     using System.Threading.Tasks;
 
-    internal class Command(string keyword, Func<ChannelChatMessageEvent, Task<bool>> useAction) {
+    internal class Command {
         public event EventHandler<PermissionLevel> PermissionLevelChanged = delegate { };
         public event EventHandler<int> UseDelayChanged = delegate { };
 
-        public string Keyword { get; private set; } = keyword;
+        public string Keyword { get; private set; }
         public PermissionLevel PermissionLevel { get => permissionLevel; set => SetPermissionLevel(value); }
         public int UseDelay { get => useDelay; set => SetUseDelay(value); }
         public DateTime LastUsed { get; private set; } = DateTime.Now;
-        public Func<ChannelChatMessageEvent, Task<bool>> UseAction = useAction;
+        public Func<ChannelChatMessageEvent, Task<bool>> UseAction;
 
         public bool IsReadyToUse => DateTime.Now > LastUsed.AddMilliseconds(UseDelay);
 
+        public Command(string keyword, Func<ChannelChatMessageEvent, Task<bool>> useAction) {
+            Logger.Info($"{nameof(Command)} | Constructor\n{nameof(keyword)}: {keyword}");
+
+            Keyword = keyword;
+            UseAction = useAction;
+        }
+
         public virtual async Task<bool?> Use(ChannelChatMessageEvent messageEvent) {
-            Logger.Info($"Using command {Keyword}.");
+            var logPrefix = $"{nameof(Command)} | {nameof(Use)}";
+            Logger.Info($"{logPrefix}\n{nameof(messageEvent)}: {JsonSerializer.Serialize(messageEvent)}");
 
             if (!IsReadyToUse) {
                 return false;
@@ -24,7 +33,7 @@
 
             var userPermissionLevel = await Permission.GetHighest(messageEvent.ChatterUserId);
             if (userPermissionLevel is null) {
-                Logger.Warning($"Could not use command {Keyword} because permission get highest attempt failed. Message event chatter user id: {messageEvent.ChatterUserId}.");
+                Logger.Warning($"{logPrefix} | {nameof(Permission.GetHighest)} result is null.");
                 return null;
             }
 
@@ -33,7 +42,7 @@
             }
 
             if (!await UseAction(messageEvent)) {
-                Logger.Warning($"Could not use command {Keyword} because use action attempt failed.");
+                Logger.Warning($"{logPrefix} | {nameof(UseAction)} result is false.");
                 return null;
             }
 
@@ -42,14 +51,14 @@
         }
 
         public void SetPermissionLevel(PermissionLevel permissionLevel) {
-            Logger.Info($"Setting permission level of command {Keyword}. Permission level: {permissionLevel}.");
+            Logger.Info($"{nameof(Command)} | {SetPermissionLevel}\n{permissionLevel}: {permissionLevel}");
 
             this.permissionLevel = permissionLevel;
             Util.InvokeDeferred(PermissionLevelChanged, PermissionLevel);
         }
 
         public void SetUseDelay(int useDelay) {
-            Logger.Info($"Setting use delay of command {Keyword}. Use delay: {useDelay}.");
+            Logger.Info($"{nameof(Command)} | {SetUseDelay}\n{useDelay}: {useDelay}");
 
             this.useDelay = useDelay;
             Util.InvokeDeferred(UseDelayChanged, UseDelay);
@@ -59,16 +68,22 @@
         private int useDelay = 1000;
     }
 
-    internal class TogglableCommand(string keyword, Func<ChannelChatMessageEvent, Task<bool>> useAction) : Command(keyword, useAction) {
+    internal class TogglableCommand : Command {
         public event EventHandler<bool> IsEnabledChanged = delegate { };
 
         private bool isEnabled = true;
         public bool IsEnabled { get => isEnabled; set => SetIsEnabled(value); }
 
-        public override async Task<bool?> Use(ChannelChatMessageEvent messageEvent) => IsEnabled ? await base.Use(messageEvent) : false;
+        public TogglableCommand(string keyword, Func<ChannelChatMessageEvent, Task<bool>> useAction) : base(keyword, useAction) => Logger.Info($"{nameof(TogglableCommand)} | Constructor\n{nameof(keyword)}: {keyword}");
+
+        public override async Task<bool?> Use(ChannelChatMessageEvent messageEvent) {
+            Logger.Info($"{nameof(TogglableCommand)} | {nameof(Use)}\n{nameof(messageEvent)}: {JsonSerializer.Serialize(messageEvent)}");
+
+            return IsEnabled ? await base.Use(messageEvent) : false;
+        }
 
         public void SetIsEnabled(bool isEnabled) {
-            Logger.Info($"Setting is enabled of command {Keyword}. Is enabled: {isEnabled}.");
+            Logger.Info($"{nameof(TogglableCommand)} | {nameof(SetIsEnabled)}\n{nameof(isEnabled)}: {isEnabled}");
 
             this.isEnabled = isEnabled;
             Util.InvokeDeferred(IsEnabledChanged, IsEnabled);
