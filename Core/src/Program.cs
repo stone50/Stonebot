444a1a7f037b369e068a82c097208e7de497bd58
Stone Hobbs <stoneh826@gmail.com>
2025-12-27 21:02:33 -0600

    Cleanup project dependencies

---
+++ Core/src/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StonebotCore.src {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class Program {
+    }
+}
