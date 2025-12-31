a43ca3285497552bf74b59cd93e250bf16bc1566
Stone Hobbs <stoneh826@gmail.com>
2025-12-30 22:19:29 -0600

    Add basic cli loop

--- CLI/src/Program.cs
+++ CLI/src/Program.cs
@@ -1,7 +1,34 @@
 ﻿namespace StonebotCLI {
     using System;
+    using System.Threading.Tasks;
 
     public static class Program {
-        public static void Main(string[] args) => Console.WriteLine("Hello, world! This is StonebotCLI!");
+        public static async Task Main(string[] args) {
+            Console.WriteLine("Welcome to Stonebot!");
+            Console.WriteLine("Use `quit` or `exit` to exit");
+            Console.WriteLine();
+            while (true) {
+                Console.Write(">Stonebot:");
+
+                var readLine = Console.ReadLine();
+                if (readLine == null) {
+                    break;
+                }
+
+                var input = readLine.Trim();
+                if (Console.IsInputRedirected) {
+                    Console.WriteLine(input);
+                }
+
+                if (
+                    input.Equals("quit", StringComparison.OrdinalIgnoreCase) ||
+                    input.Equals("exit", StringComparison.OrdinalIgnoreCase)
+                ) {
+                    break;
+                }
+
+                // TODO: process input
+            }
+        }
     }
 }
