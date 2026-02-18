112fb76be8c21a1f0bfd6163c62c86fe9cccbff5
Stone Hobbs <stoneh826@gmail.com>
2026-02-18 00:01:06 -0600

    Remove unnecessary response layer

--- Daemon/src/Endpoints/Utils.cs
+++ Daemon/src/Endpoints/Utils.cs
@@ -4,7 +4,7 @@ namespace StonebotDaemon.Endpoints {
     internal static class Utils {
         internal static IResult GetOkResult(string message) => Results.Ok(new { message });
 
-        internal static IResult GetOkResult<T>(T data) => Results.Ok(new { data });
+        internal static IResult GetOkResult<T>(T data) => Results.Ok(data);
 
         internal static IResult GetProblemResult(string detail, int statusCode) => Results.Problem(detail: detail, statusCode: statusCode);
 
