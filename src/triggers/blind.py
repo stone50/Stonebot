e7b488e68bef5f88ad48734533e932110b160915
stone50 <stoneh826@gmail.com>
2026-09-18 16:37:42 -0500

    Update blind message

--- src/triggers/blind.py
+++ src/triggers/blind.py
@@ -11,5 +11,5 @@ async def run(
     message: ChatMessage, matches: list[Any], bot: Bot, db: Connection
 ) -> None:
     await message.respond(
-        "Level difficulties (shown in red on the overlay) are based on skill/effort, not on how much time they take. Compared to chapter E, all other chapters are basically a cutscene. Compared to E18, the rest of chapter E is basically a cutscene (even Bartender and Dronelly). So the entire run is basically a build-up until E18 | strats: https://docs.google.com/document/d/1JleYdtU9Syj-5FnZQXraqlgZryAg4jnWdIdapbf5Q1E/edit?usp=sharing | spliced run: https://youtu.be/HyZNrisg4uU"
+        "Level difficulties (shown in red on the overlay) are based on skill/effort, not on how much time they take | strats: https://docs.google.com/document/d/1JleYdtU9Syj-5FnZQXraqlgZryAg4jnWdIdapbf5Q1E/edit?usp=sharing | spliced run: https://youtu.be/HyZNrisg4uU"
     )
