7ec2c2610096c6ca7bddf415e2c385c57391ce41
stone50 <stoneh826@gmail.com>
2026-09-21 20:33:41 -0500

    Update blind and overlay command messages

--- src/triggers/blind.py
+++ src/triggers/blind.py
@@ -11,5 +11,5 @@ async def run(
     message: ChatMessage, matches: list[Any], bot: Bot, db: Connection
 ) -> None:
     await message.respond(
-        "Level difficulties (shown in red on the overlay) are based on skill/effort, not on how much time they take | strats: https://docs.google.com/document/d/1JleYdtU9Syj-5FnZQXraqlgZryAg4jnWdIdapbf5Q1E/edit?usp=sharing | spliced run: https://youtu.be/HyZNrisg4uU"
+        "strats: https://docs.google.com/document/d/1JleYdtU9Syj-5FnZQXraqlgZryAg4jnWdIdapbf5Q1E/edit?usp=sharing | spliced run: https://youtu.be/HyZNrisg4uU"
     )
--- src/triggers/overlay.py
+++ src/triggers/overlay.py
@@ -11,5 +11,5 @@ async def run(
     message: ChatMessage, matches: list[Any], bot: Bot, db: Connection
 ) -> None:
     await message.respond(
-        "chat: github.com/stone50/Stonebot | inputs: github.com/stone50/Input-Overlay | splits: github.com/stone50/WYS-Blindfolded-Splitter-Overlay"
+        "Levels with red backgrounds require more skilled execution. Levels with blue text are highly reliant on RNG. | chat: github.com/stone50/Stonebot | inputs: github.com/stone50/Input-Overlay | splits: github.com/stone50/WYS-Blindfolded-Splitter-Overlay"
     )
